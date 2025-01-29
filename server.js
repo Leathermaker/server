e2ca47842b2c7e524640bbf349d71002be86fa3c
Leathermaker <leatherwebmaker123@gmail.com>
2025-01-29 13:47:45 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ import { Server } from "socket.io";
 const app = express()
 const port = 3000
 const server = createServer(app)
-const io = new Server(server,{ cors:{
-    origin: ["http://localhost:5173",'https://chatting-application-swart.vercel.app/'], // Allows all origins
-    methods: ["GET", "POST"], // Specifies allowed methods
-  },})
+const io = new Server(server,{cors:true})
 
 
   let  i = 1
