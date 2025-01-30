db9af9c63d62928a4c8498a5fd5cc2b93b8980db
Leathermaker <leatherwebmaker123@gmail.com>
2025-01-30 12:51:44 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,37 +2,41 @@ import { createServer } from "http";
 import express from "express";
 import { Server } from "socket.io";
 
+const app = express();
+const server = createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
 
-const app = express()
-const port = 3000
-const server = createServer(app)
-const io = new Server(server,{cors:true})
+io.on('connection', (socket) => {
+    console.log('a user connected:', socket.id);
+    const id = socket.id;
+    io.to(socket.id).emit('currentUser', socket.id);
 
+    socket.on('message', (msg) => {
+        let obj = {
+            msg: msg,
+            senderId: id
+        };
+        io.emit('receive', { obj }); // Broadcast the message to all clients
+    });
 
-  let  i = 1
-  let sockets = []
-  
-io.on('connection',(socket)=>{
-	console.log(socket.id,i)
-	socket.broadcast.emit('new-user',"new user joined")
-	i++
+    socket.on('file', (fileData) => {
+        console.log('File received:', fileData.fileName);
+        // Broadcast the file to all clients
+        io.emit('receive', { obj: { msg: `File: ${fileData.fileName}`, senderId: id } });
+        // You can save the file or process it further here
+    });
+	socket.on('send-file',(data)=>{
+		let fileObj = {
+		  fileName:	data.fileName,
+			fileData :  data.fileData,
+			senderId : socket.id
+		}
+		console.log("hello")
+		io.emit('file-receive',(fileObj))
+	})
+});
 
-	sockets.push(socket.id)
-	console.log(sockets.length)
-
-	socket.on('message',(msg=>{
-		console.log(msg)
-		io.emit('receive',msg)
-	}))
-
-	// socket.on('room', (data) => {
-    //     console.log(data.room)rs; 
-    //     io.to(data.room).emit('room', { msg: data.text }); // Emit back to the client
-    // });
-})
-
-server.listen(port,()=>{
-	console.log(`Sussfully connected with ${port}`)
-})
-
-
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
